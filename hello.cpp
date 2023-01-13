7641d547922a1bbb1e3bc62d669d4895d0054396
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-13 23:57:15 +0600

    first commit

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,sum;
+    cout<<"Enter two numbers: ";
+    cin>>x>>y;
+    sum=x+y;
+    cout<<"Sum is: "<<sum;
+    return 0;
+}
