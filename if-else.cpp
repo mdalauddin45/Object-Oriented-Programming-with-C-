c72bdc2724bb77297080ec026a07a50b460fa33b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-14 00:04:31 +0600

    if else add

---
+++ if-else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num;
+    cout<<"Enter Your Number ";
+    cin>>num;
+    if(num>0)
+    {
+        cout<<"Positive";
+    }
+    else
+        cout<<"Negative";
+    return 0;
+}
