ddad413122c9f985b1793b3ca238eda8ce29f025
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-01-14 00:10:03 +0600

    odd even add

---
+++ OddEven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int num;
+    cout<<"Enter any integer number ";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+        cout<<"Odd";
+    return 0;
+}
